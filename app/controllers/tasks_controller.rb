f8ba32ffab31c705595f5769e2c6c854b530b79d
Pedro Ilton <pedroilton@gmail.com>
2020-11-26 15:07:07 -0300

    Ex 2 done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    # @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new # para poder render the form
+  end
+
+  def create
+    @task = Task.create(task_params)
+
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+    # @task = Task.find(params[:id])
+  end
+
+  def update
+    # @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    # @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
